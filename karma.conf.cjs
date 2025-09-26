7316b53be918f890ab4d786018099ad1df244f49
Taha <ANH@corelia.ai>
2025-09-27 02:31:46 +0300

    To generate units tests Artifacts

--- karma.conf.cjs
+++ karma.conf.cjs
@@ -7,25 +7,38 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
+      require('karma-junit-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
+
     client: {
       jasmine: {},
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
+
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage'),
       subdir: '.',
       reporters: [
         { type: 'html' },
+        { type: 'lcov' },
         { type: 'text-summary' }
       ]
     },
-    reporters: ['progress', 'kjhtml'],
+
+    junitReporter: {
+      outputDir: 'reports/unit',      // results will be saved here
+      outputFile: 'test-results.xml', // single file
+      useBrowserName: false           // don’t append browser name
+    },
+
+    reporters: ['progress', 'kjhtml', 'junit', 'coverage'],
+
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
+
     browsers: ['ChromeHeadlessNoSandbox'],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
@@ -33,7 +46,8 @@ module.exports = function (config) {
         flags: ['--no-sandbox', '--disable-setuid-sandbox']
       }
     },
-    singleRun: false,
-    restartOnFileChange: true
+
+    singleRun: true, // ✅ for CI, run once and exit
+    restartOnFileChange: false
   });
 };
